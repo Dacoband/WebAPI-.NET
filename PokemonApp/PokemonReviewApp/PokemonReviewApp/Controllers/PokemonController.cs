5912992eff0b9a7130caab3d59758250c9941aec
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-01-20 01:59:18 +0700

    Update 20/1
    Create PokemonDto

--- PokemonApp/PokemonReviewApp/PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonApp/PokemonReviewApp/PokemonReviewApp/Controllers/PokemonController.cs
@@ -1,6 +1,8 @@
 ﻿
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PokemonReviewApp.Data;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interface;
 using PokemonReviewApp.Models;
 
@@ -13,24 +15,58 @@ namespace PokemonReviewApp.Controllers
     public class PokemonController : Controller
     {
         private readonly IPokemonRespository _pokemonRespository;
-        private readonly DataContext context;
+        private readonly IMapper _mapper;
+        
 
-        public PokemonController(IPokemonRespository pokemonRespository, DataContext context)
+        public PokemonController(IPokemonRespository pokemonRespository, IMapper mapper )
         {
             _pokemonRespository = pokemonRespository;
-            this.context = context;
+            _mapper = mapper;
+            
         }
 
         [HttpGet]
-        [ProducesResponseType(200, Type  = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
 
         public IActionResult GetPokemons()
         {
-            var pokemons = _pokemonRespository.GetPokemons();
+            var pokemons =_mapper.Map<List<PokemonDto>>(_pokemonRespository.GetPokemons());
+
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
+
             return Ok(pokemons);
+        }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRespository.PokemonExists(pokeId))
+                return NotFound();
+
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRespository.GetPokemon(pokeId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemon);
+        }
+
+        [HttpGet("pokeId/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)] 
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRespository.PokemonExists(pokeId))
+                return NotFound();
+            var rating = _pokemonRespository.GetPokemonRating(pokeId);
+
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(rating);
         }
     }
 }
--- PokemonApp/PokemonReviewApp/PokemonReviewApp/Interface/IPokemonRespository.cs
+++ PokemonApp/PokemonReviewApp/PokemonReviewApp/Interface/IPokemonRespository.cs
@@ -5,6 +5,10 @@ namespace PokemonReviewApp.Interface
     public interface IPokemonRespository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
 
     }
 }
